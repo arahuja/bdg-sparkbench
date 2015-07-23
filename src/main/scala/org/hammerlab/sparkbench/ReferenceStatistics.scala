4871b2b2f98188b65789bc0cda04d22199113b8a
Arun Ahuja <aahuja11@gmail.com>
2015-07-23 13:33:29 -0400

    add commandline parsing and sort option

--- src/main/scala/org/hammerlab/sparkbench/ReferenceStatistics.scala
+++ src/main/scala/org/hammerlab/sparkbench/ReferenceStatistics.scala
@@ -1,19 +1,43 @@
 package org.hammerlab.sparkbench
 
 import org.apache.spark.{SparkContext, SparkConf}
+import org.kohsuke.args4j.{Option => Args4jOption, CmdLineException, CmdLineParser}
 
 object ReferenceStatistics extends App {
+  
+  @Args4jOption(name="--reference", required = true, usage="HDFS path to FASTA file")
+  var referencepath: String = ""
+
+
+  @Args4jOption(name="--outputPath", required = true, usage="Output path to save substring counts")
+  var outputPath: String = ""
+
+  @Args4jOption(name="--substringLength", usage="Length of substrings to create")
+  var substringLength: Int = 30
+
+  @Args4jOption(name="--sort", usage="Sort the substrings by count before outputing")
+  var sort: Boolean = false
 
   override def main(args: Array[String]) = {
     val conf = new SparkConf()
       .setAppName("ReferenceStatistics")
     val sc = new SparkContext(conf)
+
+    val parser = new CmdLineParser(this)
+    parser.setUsageWidth(150);
+
+    try {
+      parser.parseArgument(args:_*)
+    } catch {
+      case e: CmdLineException =>
+        println(e.getMessage)
+    }
     
-    val referenceFile = args(1)
-    val substringLength = args(2).toInt
-    val outputPath = args(3)
-    
-    val lines = sc.textFile(referenceFile).zipWithIndex()
+    val lines = sc
+      .textFile(referencepath)
+      // Skip contig description lines
+      .filter(line => !line.startsWith(">"))
+      .zipWithIndex()
 
     val substringsAndPositions =
       lines
@@ -25,10 +49,16 @@ object ReferenceStatistics extends App {
               .map(s => (s._1, (s._2, lineAndIndex._2)))
         )
 
-    val substringCounts = 
+    var substringCounts = 
       substringsAndPositions
       .map(kv => (kv._1, 1L))
       .reduceByKey(_ + _)
+
+    substringCounts = 
+      if (sort) 
+        substringCounts.sortBy(_._2)
+      else
+        substringCounts
 
     substringCounts
       .map(kv => s"${kv._1},${kv._2}")
